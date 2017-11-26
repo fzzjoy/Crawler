00a6ec1846ef590637a45cd9e7240271603ca249
zhouzhou fang <zhouzhou fang>
2017-11-26 14:47:24 +0800

    init:

---
+++ crawling/Throttle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on 2017-11-11
+
+@author: Joy
+'''
+import datetime
+import time
+from future.backports.urllib.parse import urlparse
+
+class Throttle(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, delay):
+        '''
+        Constructor
+        '''
+        self.delay = delay 
+        self.domains = {}
+        
+    def wait(self, url):
+        domain = urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+        
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.datetime.now()
---
+++ crawling/scraping.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on 2017-11-12
+
+@author: Joy
+'''
+from crawling import download
+import time
+
+FIELDS = ('area', 'population', 'iso', 'country', 'capital',
+         'continent', 'tld', 'currency_code', 'currency_name', 
+         'phone', 'postal_code_format', 'postal_code_regex', 
+         'languages', 'neighbours'
+         )
+
+import re
+def re_scraper(html):
+    results = {}
+    for field in FIELDS:
+        searchResult = re.search('<tr id="places_%s__row">.*?<td class="w2p_fw">(.*?)</td>' % field, html)
+        if searchResult is None:
+            results[field] = None
+            print "not find [%s] informations!" % field
+        else:
+            results[field] = searchResult.groups()[0]
+#             print "%s: [%s]" % (field, results[field])
+    return results
+    
+
+from bs4 import BeautifulSoup
+def bs_scraper(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    results = {}
+    for field in FIELDS:
+        findResult = soup.find('table')
+        if findResult is not None:
+            findResult = findResult.find('tr', id = 'places_%s__row' % field)
+            if findResult is not None:
+                findResult = findResult.find('td', class_= 'w2p_fw')
+                if findResult is not None:
+                    results[field] = findResult.text
+#                     print "%s: [%s]" % (field, results[field])
+#     print results
+    return results
+
+import lxml.html
+def lxml_scraper(html):
+    tree = lxml.html.fromstring(html)
+    results = {}
+    for field in FIELDS:
+        selectResult = tree.cssselect('table > tr#places_%s__row > td.w2p_fw' % field)
+        if selectResult is None:
+            print "not find [%s] informations!" % field
+        else:
+            results[field] = selectResult[0].text_content()
+    return results
+
+if __name__ == '__main__':
+    NUM_ITERATIONS = 1000
+    html = download('http://example.webscraping.com/places/default/view/1')
+    if html is None:
+        exit(0)   
+    for name, scraper in [('Regular expressions',re_scraper),
+                          ('BeautifulSoup', bs_scraper),
+                          ('Lxml', lxml_scraper)]:
+        start = time.time()
+        for i in range(NUM_ITERATIONS):
+            if scraper == re_scraper:
+                re.purge() # 清除缓存
+            result = scraper(html)
+            if (result['area'] != '647,500 square kilometres'):
+                print "%s scraper html has error happend!"
+        end = time.time()
+        print '%s: %.2f seconds' %(name, end - start)
+    print "Hello python"
